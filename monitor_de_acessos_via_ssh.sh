4a7326ebf35c896bf0d8ce95e47fa7bd79c917c1
Marina Hedlund Jardim <84291180+M705-bit@users.noreply.github.com>
2025-05-08 07:29:32 -0300

    Create monitor_de_acessos_via_ssh.sh

---
+++ monitor_de_acessos_via_ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+last -s yesterday | awk -F " " 'NR > 1 {print $3}' | sort | uniq -c > saida_padrão.txt
+VAR="/home/lydians/saida_padrão.txt"
+MAX=$(awk 'BEGIN { max = -inf }{ if ($1>max) max = $1} END { print max }' "$VAR")
+awk -v max="$MAX" '$1 == max { print "Parabéns " $2 ", você é o usuário que mais acessou esta VM via ssh, foram um total de " $1 " vezes!" }' "$VAR"
